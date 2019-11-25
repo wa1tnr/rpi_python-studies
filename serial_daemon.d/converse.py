a8a459b2a9cd3fae6497d5467b1609c279f63ec0
wa1tnr <wa1tnr@users.noreply.github.com>
2019-11-25 16:07:57 +0000

    one typo and maybe others

--- serial_daemon.d/converse.py
+++ serial_daemon.d/converse.py
@@ -8,7 +8,7 @@ sys.stdout.write('H')
 sys.stdout.flush()
 
 with serial.Serial(port='/dev/ttyS0', baudrate=38400, bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=0, rtscts=0) as ser:
-    rutroh=0
+    rutroh = 0
     while -1:
         x = ser.read()
 
@@ -16,7 +16,7 @@ with serial.Serial(port='/dev/ttyS0', baudrate=38400, bytesize=8, parity='N', st
 
         while (xp[-2:-1] == "+"):
             print('+', end='', flush=True)
-            rutroh = rutroh + 1
+            rutroh = rutroh + 1 # require three consecutive +'s to trigger an ESCape
             if (rutroh == 3):
                 rutroh = 0
                 print("SYSTEM ESCAPE: +++ detected.", flush=True)
@@ -32,9 +32,11 @@ with serial.Serial(port='/dev/ttyS0', baudrate=38400, bytesize=8, parity='N', st
             x = ser.read()
             xp = str(x)
 
-        rutroh = 0
+        rutroh = 0 # enforce three consecutive +'s requirement
+        # if it's any char except CR or LF, print it
         if ((xp[-3:-1] != "\\n") & (xp[-3:-1] != "\\r")):
             print (xp[-2:-1], end='', flush=True)
 
+        # if it was a newline, print its equivalent
         if (xp[-3:-1] == "\\n"):
             print("", flush=True)
